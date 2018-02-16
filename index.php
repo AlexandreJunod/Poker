635fc0f43e0cf7382cfd744efa38dc04fb5a88c5
AlexandreJunod <36194606+AlexandreJunod@users.noreply.github.com>
2018-02-16 14:58:05 +0100

    Uses cases v.3 rendu + html/css index.php
    Date du rendu des uses cases.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="includes/style.css" />
+        <title>Poker online</title>
+    </head>
+    <body>
+        <div class="FormContainer">
+            <div class="FormTitle">Poker online</div>
+            <div class="FormDesign">
+                <div class="FormFields">Pseudo [CHAMP DE TEXTE] <br><br><br><br> mot de passe [CHAMP DE TEXTE]</div>
+                <div class="FormButton">[connexion]</div>
+            </div>
+            <div class="FormLink"><a href="NewAccount.php">Pas encore de compte ? Inscrivez-vous !</a></div>
+        </div>
+    </body>
+</html>
