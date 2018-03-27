a297fdf5271591d5c61eaca723507d295a457311
AlexandreJunod <36194606+AlexandreJunod@users.noreply.github.com>
2018-03-27 15:07:56 +0200

    Commit 20 - suite
    Il y a eu un problème dans le commit, tout ne c'est pas envoyé

--- Documentation/Poker.sql
+++ Documentation/Poker.sql
@@ -13,73 +13,72 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 DROP SCHEMA IF EXISTS poker;
 CREATE SCHEMA poker ;
 
-CREATE TABLE IF NOT EXISTS Poker.Joueur (
-  idJoueur INT(11) NOT NULL AUTO_INCREMENT,
-  PseudoJoueur VARCHAR(45) NOT NULL,
-  MotDePasseJoueur VARCHAR(45) NOT NULL,
-  PRIMARY KEY (idJoueur),
-  UNIQUE INDEX PseudoJoueur_UNIQUE (PseudoJoueur ASC))
+CREATE TABLE IF NOT EXISTS Poker.Player (
+  idPlayer INT(11) NOT NULL AUTO_INCREMENT,
+  PseudoPlayer VARCHAR(45) NOT NULL,
+  PasswordPlayer VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idPlayer),
+  UNIQUE INDEX PseudoPlayer_UNIQUE (PseudoPlayer ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS Poker.Siege (
-  idSiege INT(11) NOT NULL AUTO_INCREMENT,
-  ArgentSiege INT(11) NULL DEFAULT NULL,
-  MainSiege VARCHAR(45) NULL DEFAULT NULL,
-  fkPartieSiege INT(11) NOT NULL,
-  fkEtatSiege INT(11) NOT NULL,
-  fkJoueurSiege INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (idSiege),
-  INDEX fk_Siege_Partie1_idx (fkPartieSiege ASC),
-  INDEX fk_Siege_Etat1_idx (fkEtatSiege ASC),
-  INDEX fk_Siege_Joueur1_idx (fkJoueurSiege ASC),
-  UNIQUE INDEX fkJoueurSiege_UNIQUE (fkJoueurSiege ASC),
-  CONSTRAINT fk_Siege_Partie1
-    FOREIGN KEY (fkPartieSiege)
-    REFERENCES Poker.Partie (idPartie)
+CREATE TABLE IF NOT EXISTS Poker.Seat (
+  idSeat INT(11) NOT NULL AUTO_INCREMENT,
+  MoneySeat INT(11) NULL DEFAULT NULL,
+  HandSeat VARCHAR(45) NULL DEFAULT NULL,
+  fkGameSeat INT(11) NOT NULL,
+  fkStatusSeat INT(11) NOT NULL,
+  fkPlayerSeat INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (idSeat),
+  INDEX fk_Seat_Game1_idx (fkGameSeat ASC),
+  INDEX fk_Seat_Status1_idx (fkStatusSeat ASC),
+  INDEX fk_Seat_Player1_idx (fkPlayerSeat ASC),
+  UNIQUE INDEX fkPlayerSeat_UNIQUE (fkPlayerSeat ASC),
+  CONSTRAINT fk_Seat_Game1
+    FOREIGN KEY (fkGameSeat)
+    REFERENCES Poker.Game (idGame)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT fk_Siege_Etat1
-    FOREIGN KEY (fkEtatSiege)
-    REFERENCES Poker.Etat (idEtat)
+  CONSTRAINT fk_Seat_Status1
+    FOREIGN KEY (fkStatusSeat)
+    REFERENCES Poker.Status (idStatus)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT fk_Siege_Joueur1
-    FOREIGN KEY (fkJoueurSiege)
-    REFERENCES Poker.Joueur (idJoueur)
+  CONSTRAINT fk_Seat_Player1
+    FOREIGN KEY (fkPlayerSeat)
+    REFERENCES Poker.Player (idPlayer)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS Poker.Partie (
-  idPartie INT(11) NOT NULL AUTO_INCREMENT,
-  PotPartie INT(11) NULL DEFAULT NULL,
-  BoardPartie VARCHAR(45) NULL DEFAULT NULL,
-  BlindPartie INT(11) NULL DEFAULT NULL,
-  DealerPartie INT(11) NOT NULL DEFAULT '1',
-  SmallBlindPartie INT(11) NOT NULL DEFAULT '2',
-  BigBlindPartie INT(11) NOT NULL DEFAULT '3',
-  HeureDebutPartie TIME NULL DEFAULT NULL,
-  PRIMARY KEY (idPartie))
+CREATE TABLE IF NOT EXISTS Poker.Game (
+  idGame INT(11) NOT NULL AUTO_INCREMENT,
+  PotGame INT(11) NULL DEFAULT NULL,
+  BoardGame VARCHAR(45) NULL DEFAULT NULL,
+  BlindGame INT(11) NULL DEFAULT NULL,
+  DealerGame INT(11) NOT NULL DEFAULT '1',
+  OrderGame INT(11) NULL DEFAULT NULL,
+  HourStartGame TIME NULL DEFAULT NULL,
+  PRIMARY KEY (idGame))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS Poker.Etat (
-  idEtat INT(11) NOT NULL AUTO_INCREMENT,
-  DescriptionEtat VARCHAR(45) NOT NULL,
-  PRIMARY KEY (idEtat),
-  UNIQUE INDEX DescriptionEtat_UNIQUE (DescriptionEtat ASC))
+CREATE TABLE IF NOT EXISTS Poker.Status (
+  idStatus INT(11) NOT NULL AUTO_INCREMENT,
+  DescriptionStatus VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idStatus),
+  UNIQUE INDEX DescriptionStatus_UNIQUE (DescriptionStatus ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS Poker.Parametres (
-  idParametres INT(11) NOT NULL AUTO_INCREMENT,
-  NomParametres VARCHAR(45) NOT NULL,
-  ValeurInt INT(11) NULL DEFAULT NULL,
-  ValeurDate DATETIME NULL DEFAULT NULL,
-  ValeurChar VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (idParametres))
+CREATE TABLE IF NOT EXISTS Poker.Settings (
+  idSettings INT(11) NOT NULL AUTO_INCREMENT,
+  NameSettings VARCHAR(45) NOT NULL,
+  ValueInt INT(11) NULL DEFAULT NULL,
+  ValueDate DATETIME NULL DEFAULT NULL,
+  ValueChar VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (idSettings))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -87,26 +86,26 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
-INSERT INTO poker.partie (PotPartie, BlindPartie) VALUES ('0', '3000');
-INSERT INTO poker.etat (DescriptionEtat) VALUES ('En attente');
-INSERT INTO poker.etat (DescriptionEtat) VALUES ('En jeu');
-INSERT INTO poker.etat (DescriptionEtat) VALUES ('Doit jouer');
-INSERT INTO poker.siege (fkPartieSiege, fkEtatSiege) VALUES ('1', '1');
-INSERT INTO poker.siege (fkPartieSiege, fkEtatSiege) VALUES ('1', '1');
-INSERT INTO poker.siege (fkPartieSiege, fkEtatSiege) VALUES ('1', '1');
-INSERT INTO poker.siege (fkPartieSiege, fkEtatSiege) VALUES ('1', '1');
-INSERT INTO poker.siege (fkPartieSiege, fkEtatSiege) VALUES ('1', '1');
-INSERT INTO poker.siege (fkPartieSiege, fkEtatSiege) VALUES ('1', '1');
-INSERT INTO poker.parametres (NomParametres, ValeurInt) VALUES ('TempsAugmentationBlind', '5');
+INSERT INTO poker.Game (PotGame, BlindGame) VALUES ('0', '3000');
+INSERT INTO poker.Status (DescriptionStatus) VALUES ('Waiting');
+INSERT INTO poker.Status (DescriptionStatus) VALUES ('In game');
+INSERT INTO poker.Status (DescriptionStatus) VALUES ('Playing');
+INSERT INTO poker.Seat (fkGameSeat, fkStatusSeat) VALUES ('1', '1');
+INSERT INTO poker.Seat (fkGameSeat, fkStatusSeat) VALUES ('1', '1');
+INSERT INTO poker.Seat (fkGameSeat, fkStatusSeat) VALUES ('1', '1');
+INSERT INTO poker.Seat (fkGameSeat, fkStatusSeat) VALUES ('1', '1');
+INSERT INTO poker.Seat (fkGameSeat, fkStatusSeat) VALUES ('1', '1');
+INSERT INTO poker.Seat (fkGameSeat, fkStatusSeat) VALUES ('1', '1');
+INSERT INTO poker.Settings (NameSettings, ValueInt) VALUES ('TimeToIncreaseBlind', '5');
 
--- Ajout de 9 joueurs, afin de pouvoir tester sans avoir a recréer à chaque fois les joueurs
-INSERT INTO poker.joueur (PseudoJoueur, MotDePasseJoueur) VALUES ('Alexandre', PASSWORD('Pa$$w0rd'));
-INSERT INTO poker.joueur (PseudoJoueur, MotDePasseJoueur) VALUES ('Alexandre2', PASSWORD('Pa$$w0rd'));
-INSERT INTO poker.joueur (PseudoJoueur, MotDePasseJoueur) VALUES ('Alexandre3', PASSWORD('Pa$$w0rd'));
-INSERT INTO poker.joueur (PseudoJoueur, MotDePasseJoueur) VALUES ('Alexandre4', PASSWORD('Pa$$w0rd'));
-INSERT INTO poker.joueur (PseudoJoueur, MotDePasseJoueur) VALUES ('Alexandre5', PASSWORD('Pa$$w0rd'));
-INSERT INTO poker.joueur (PseudoJoueur, MotDePasseJoueur) VALUES ('Alexandre6', PASSWORD('Pa$$w0rd'));
-INSERT INTO poker.joueur (PseudoJoueur, MotDePasseJoueur) VALUES ('Alexandre7', PASSWORD('Pa$$w0rd'));
-INSERT INTO poker.joueur (PseudoJoueur, MotDePasseJoueur) VALUES ('Alexandre8', PASSWORD('Pa$$w0rd'));
-INSERT INTO poker.joueur (PseudoJoueur, MotDePasseJoueur) VALUES ('Alexandre9', PASSWORD('Pa$$w0rd'));
+-- ADD 9 Players, using for test
+INSERT INTO poker.Player (PseudoPlayer, PasswordPlayer) VALUES ('Alexandre', PASSWORD('Pa$$w0rd'));
+INSERT INTO poker.Player (PseudoPlayer, PasswordPlayer) VALUES ('Alexandre2', PASSWORD('Pa$$w0rd'));
+INSERT INTO poker.Player (PseudoPlayer, PasswordPlayer) VALUES ('Alexandre3', PASSWORD('Pa$$w0rd'));
+INSERT INTO poker.Player (PseudoPlayer, PasswordPlayer) VALUES ('Alexandre4', PASSWORD('Pa$$w0rd'));
+INSERT INTO poker.Player (PseudoPlayer, PasswordPlayer) VALUES ('Alexandre5', PASSWORD('Pa$$w0rd'));
+INSERT INTO poker.Player (PseudoPlayer, PasswordPlayer) VALUES ('Alexandre6', PASSWORD('Pa$$w0rd'));
+INSERT INTO poker.Player (PseudoPlayer, PasswordPlayer) VALUES ('Alexandre7', PASSWORD('Pa$$w0rd'));
+INSERT INTO poker.Player (PseudoPlayer, PasswordPlayer) VALUES ('Alexandre8', PASSWORD('Pa$$w0rd'));
+INSERT INTO poker.Player (PseudoPlayer, PasswordPlayer) VALUES ('Alexandre9', PASSWORD('Pa$$w0rd'));
 
